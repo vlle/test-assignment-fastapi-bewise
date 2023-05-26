41e1d5365916acf9286ac566e3720d8748575628
artemiikulikov <aapqwhat@gmail.com>
2023-05-26 15:35:10 +0300

    :rotating_light: fix black linter

--- p1/app/test_variable.py
+++ p1/app/test_variable.py
@@ -2,6 +2,4 @@ import os
 
 # do not touch, this file is needed for test_start.sh
 # we are ovveriding env variable to test api on another postgres instance
-os.environ[
-    "DATABASE_URL"
-] = "postgresql+psycopg://postgres:postgres@test:5432/postgres"
+os.environ["DATABASE_URL"] = "postgresql+psycopg://postgres:postgres@test:5432/postgres"
--- p2/app/test_variable.py
+++ p2/app/test_variable.py
@@ -1,6 +1,4 @@
 import os
 
 # we are ovveriding env variable to test api on another postgres instance
-os.environ[
-    "DATABASE_URL"
-] = "postgresql+psycopg://postgres:postgres@test:5432/postgres"
+os.environ["DATABASE_URL"] = "postgresql+psycopg://postgres:postgres@test:5432/postgres"
