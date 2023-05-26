df217fd53e7ec78cc74f212f13a2ed2423c8ba3e
artemiikulikov <aapqwhat@gmail.com>
2023-05-26 12:15:30 +0300

    :white_check_mark: add cp .env

--- p1/test_start.sh
+++ p1/test_start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+cp env.example app/.env
 docker pull postgres
 docker run -d --name test  -p 5432:5432  -e POSTGRES_USER=postgres  -e POSTGRES_PASSWORD=postgres  -e POSTGRES_DB=postgres postgres -N 2000
 python3 -m venv venv
