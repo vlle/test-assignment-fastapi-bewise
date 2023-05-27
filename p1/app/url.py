72c7945947c954364d7bd976133016064bc61a0e
artemiikulikov <aapqwhat@gmail.com>
2023-05-28 01:08:15 +0300

    :heavy_minus_sign: rm url.py

--- p1/app/url.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlencode, urljoin, urlparse
-
-QUIZ_LINK = "https://jservice.io/api/random"
-a = urlparse(QUIZ_LINK)
-query = urlencode({"count": 3})
-api_url_with_qustion_num = urljoin(QUIZ_LINK, query, allow_fragments=False)
-print(api_url_with_qustion_num)
